7d2c14f9db358275a926c7d496324920fa61d40c
Kaartik Shukla <kaartikshukla7120@gmail.com>
2024-10-20 18:45:11 +0530

    Added songList and songPlayer stack screens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,7 +6,7 @@ import { useEffect } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
-
+import { PaperProvider } from 'react-native-paper';
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
@@ -28,10 +28,19 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
+      <PaperProvider>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+          <Stack.Screen name="songList" initialParams={{
+            title: '',
+            subtitle: '',
+            id: ''
+          }}
+          />
+          <Stack.Screen name="songPlayer" />
+        </Stack>
+      </PaperProvider>
     </ThemeProvider>
   );
 }
